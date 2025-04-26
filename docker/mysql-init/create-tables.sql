82017a0ef1bd95d972dfc2ddd09b3ef5019b6d24
DucAn <annguyen2422004@gmail.com>
2025-04-27 00:45:54 +0700

    Refactor SQL table definitions: standardize column names for product categories, customer locations, and customers tables.

--- docker/mysql-init/create-tables.sql
+++ docker/mysql-init/create-tables.sql
@@ -3,23 +3,23 @@
 DROP TABLE IF EXISTS product_categories;
 -- Create product categories table
 CREATE TABLE  product_categories (
-    id VARCHAR(10) PRIMARY KEY,
-    category VARCHAR(50) NOT NULL,
-    subcategory VARCHAR(50) NOT NULL,
-    maintenance VARCHAR(5)
+    ID VARCHAR(10) PRIMARY KEY,
+    CAT VARCHAR(50) NOT NULL,
+    SUBCAT VARCHAR(50) NOT NULL,
+    MAINTENANCE VARCHAR(5)
 );
 
 DROP TABLE IF EXISTS customer_locations;
 -- Create customer locations table
 CREATE TABLE customer_locations (
-    customer_id VARCHAR(20) PRIMARY KEY,
-    country VARCHAR(50) NOT NULL
+    CID VARCHAR(20) PRIMARY KEY,
+    CNTRY VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS customers;
 -- Create customer information table
 CREATE TABLE customers (
-    customer_id VARCHAR(20) PRIMARY KEY,
-    birth_date VARCHAR(50),
-    gender VARCHAR(10)
+    CID VARCHAR(20) PRIMARY KEY,
+    BDATE VARCHAR(50),
+    GEN VARCHAR(10)
 );
