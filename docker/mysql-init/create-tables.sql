0ceb7a8202ded942e5d1a8113b5ee50492427913
DucAn <annguyen2422004@gmail.com>
2025-04-28 00:42:24 +0700

    Standardize column names in create-tables.sql for product categories, customer locations, and customers; add create-dwh-db.sql for DWH database setup and grant privileges.

--- docker/mysql-init/create-tables.sql
+++ docker/mysql-init/create-tables.sql
@@ -3,23 +3,23 @@
 DROP TABLE IF EXISTS product_categories;
 -- Create product categories table
 CREATE TABLE  product_categories (
-    ID VARCHAR(10) PRIMARY KEY,
-    CAT VARCHAR(50) NOT NULL,
-    SUBCAT VARCHAR(50) NOT NULL,
-    MAINTENANCE VARCHAR(5)
+    id VARCHAR(10) PRIMARY KEY,
+    cat VARCHAR(50) NOT NULL,
+    subcat VARCHAR(50) NOT NULL,
+    maintenance VARCHAR(5)
 );
 
 DROP TABLE IF EXISTS customer_locations;
 -- Create customer locations table
 CREATE TABLE customer_locations (
-    CID VARCHAR(20) PRIMARY KEY,
-    CNTRY VARCHAR(50) NOT NULL
+    cid VARCHAR(20) PRIMARY KEY,
+    cntry VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS customers;
 -- Create customer information table
 CREATE TABLE customers (
-    CID VARCHAR(20) PRIMARY KEY,
-    BDATE VARCHAR(50),
-    GEN VARCHAR(10)
+    cid VARCHAR(20) PRIMARY KEY,
+    bdate VARCHAR(50),
+    gen VARCHAR(10)
 );
---
+++ docker/postgres-init/create-dwh-db.sql
@@ -0,0 +1,3 @@
+-- Create the DWH database for ETL pipeline
+CREATE DATABASE dwh;
+GRANT ALL PRIVILEGES ON DATABASE dwh TO airflow;
